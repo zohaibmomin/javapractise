f9f1c9ee37bdf32e26b19dbbd6e53dad21910050
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-05-06 16:39:40 +0530

    Arrays - Min Max logic

--- JavaPractise/Lession1/Subscribe/src/ArraySumAverage.java
+++ JavaPractise/Lession1/Subscribe/src/ArraySumAverage.java
@@ -4,9 +4,26 @@ import java.util.Scanner;
 public class ArraySumAverage {
     public static void main(String[] args) {
         System.out.println("Welcome to sum and average problem");
-        int[] arr = {1,2,3,88,9,5,3,8,7,3,5,1,2,3};
+        int[] arr = {1,2,3,88,9,5,3,8,7,3,5,1,2,3,-199,200,2,198,-1,5};
         printSumAvg(arr);
         printOccurences(arr);
+        printMax(arr);
+    }
+
+    private static void printMax(int[] arr) {
+        int max , min;
+        max=min=arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+            }
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("Max is " + max);
+        System.out.println("Min is " + min);
     }
 
     private static void printOccurences(int[] arr) {
