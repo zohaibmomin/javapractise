5dd383df8f00b188bd7a24f95a1c7ba1af2cfcbc
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-05-14 18:53:47 +0530

    Packages - Access modifiers public,default,private

--- JavaPractise/Lession1/Subscribe/src/in/javapractise/Sample.java
+++ JavaPractise/Lession1/Subscribe/src/in/javapractise/Sample.java
@@ -3,5 +3,13 @@ package in.javapractise;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Car mycar = new Car();
+        mycar.setColor("Red");
+//        mycar.setCost(999); //since setter is kept as private access I cannot do this
+        mycar.setModel("Swift");
+        System.out.println(mycar);
+
+        Car secondCar = new Car("Black","BMW",1,5000);
+        System.out.println(secondCar);
     }
 }
--- JavaPractise/Lession1/Subscribe/src/in/secondjavapractise/Sample.java
+++ JavaPractise/Lession1/Subscribe/src/in/secondjavapractise/Sample.java
@@ -1,5 +1,7 @@
 package in.secondjavapractise;
 
+import in.javapractise.Car;
+
 public class Sample {
     public static void main(String[] args) {
         in.javapractise.Sample sc = new in.javapractise.Sample();
@@ -7,5 +9,15 @@ public class Sample {
         in.secondjavapractise.Sample sc2 = new in.secondjavapractise.Sample();
         System.out.println("Hello Second java practise : " + sc2);
 
+
+        //Trying to access Class Car in 1st package
+        System.out.println("Hello");
+        Car mycar = new Car();
+        mycar.setColor("Grey");//Able to access Color using setter because property color is private
+//        mycar.model = "Swift"; Cannot access because model is set default
+        System.out.println(mycar);
+
+        Car secondCar = new Car("Black","BMW",1,5000);
+        System.out.println(secondCar);
     }
 }
