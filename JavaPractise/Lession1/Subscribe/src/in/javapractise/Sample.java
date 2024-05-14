cf3f2e3c5f8487693baa604d398e93d3dad2a449
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-05-14 16:45:09 +0530

    Packages - Basic sample

---
+++ JavaPractise/Lession1/Subscribe/src/in/javapractise/Sample.java
@@ -0,0 +1,7 @@
+package in.javapractise;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ JavaPractise/Lession1/Subscribe/src/in/secondjavapractise/Sample.java
@@ -0,0 +1,11 @@
+package in.secondjavapractise;
+
+public class Sample {
+    public static void main(String[] args) {
+        in.javapractise.Sample sc = new in.javapractise.Sample();
+        System.out.println("Hello first java practise : " + sc);
+        in.secondjavapractise.Sample sc2 = new in.secondjavapractise.Sample();
+        System.out.println("Hello Second java practise : " + sc2);
+
+    }
+}
