b57e3bd131c970c5d6ba672e308c22d21448cd6f
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-18 14:15:10 +0530

    Springboot Startup - Dependencies - Lambok, Spring Web, Java 22, Maven , Spring 3.3.0

---
+++ JavaPractise/zohaib-spring-boot/src/main/java/com/javapractise/ZohaibSpringBoot/zohaib_spring_boot/ZohaibSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javapractise.ZohaibSpringBoot.zohaib_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohaibSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZohaibSpringBootApplication.class, args);
+	}
+
+}
