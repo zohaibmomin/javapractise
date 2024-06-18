a8cbcdf48931525ba29de6835e09e45c4fe7da00
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-18 15:26:50 +0530

    Dependency Injection - Autowired, ConditionOnProperty env variable

--- demospringboot/src/main/java/com/javapractise/demospringboot/demospringboot/configuration/AppConfig.java
+++ demospringboot/src/main/java/com/javapractise/demospringboot/demospringboot/configuration/AppConfig.java
@@ -1,14 +1,23 @@
 package com.javapractise.demospringboot.demospringboot.configuration;
 
 import com.javapractise.demospringboot.demospringboot.DB;
+import com.javapractise.demospringboot.demospringboot.DevDB;
 import com.javapractise.demospringboot.demospringboot.ProdDB;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public DB getData(){
+    @ConditionalOnProperty(name = "env", havingValue = "PROD")
+    public DB getProdData(){
         return new ProdDB();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "env", havingValue = "DEV")
+    public DB getDevData(){
+        return new DevDB();
     }
 }
