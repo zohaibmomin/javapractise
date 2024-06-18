83934af487071eaaa356fa9782f19a991fbd5c56
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-18 20:47:31 +0530

    Code Cleanup

--- demospringboot/src/main/java/com/javapractise/demospringboot/demospringboot/services/EmployeeService.java
+++ demospringboot/src/main/java/com/javapractise/demospringboot/demospringboot/services/EmployeeService.java
@@ -3,14 +3,11 @@ package com.javapractise.demospringboot.demospringboot.services;
 import com.javapractise.demospringboot.demospringboot.dto.EmployeeDTO;
 import com.javapractise.demospringboot.demospringboot.entity.EmployeeEntity;
 import com.javapractise.demospringboot.demospringboot.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 @Service
 public class EmployeeService {
