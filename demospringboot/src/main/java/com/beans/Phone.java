f7158851dabd75028896e894508a7c11aeedda33
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-25 13:13:27 +0530

    Dependency Injection - Setter based

--- demospringboot/src/main/java/com/beans/Phone.java
+++ demospringboot/src/main/java/com/beans/Phone.java
@@ -3,14 +3,19 @@ package com.beans;
 public class Phone {
     public Network network;
 
-    public Phone(Network network) {
-        System.out.println("Phone constructor..");
-        this.network = network;
-    }
+//    public Phone(Network network) {
+//        System.out.println("Phone constructor..");
+//        this.network = network;
+//    }
+//
+//    public Phone(int a, double b){
+//        System.out.println("Phone parameterised constructor...");
+//        System.out.println(a);
+//        System.out.println(b);
+//    }
 
-    public Phone(int a, double b){
-        System.out.println("Phone parameterised constructor...");
-        System.out.println(a);
-        System.out.println(b);
+    public void setNetwork(Network network) {
+        System.out.println("Inside network setter...");
+        this.network = network;
     }
 }
