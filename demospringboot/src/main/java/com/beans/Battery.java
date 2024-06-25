c4fd4c65878bb702ab08f03c17cbd055f60ebbe5
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-25 11:41:12 +0530

    Bean Lifecycle - Using interface InitializingBean, DisposableBean

--- demospringboot/src/main/java/com/beans/Battery.java
+++ demospringboot/src/main/java/com/beans/Battery.java
@@ -2,20 +2,27 @@ package com.beans;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Battery {
+public class Battery implements InitializingBean, DisposableBean {
     public Battery() {
         System.out.println("Bean Lifecycle ::  Beans battery Constructor initialised....");
     }
-    @PostConstruct
+//    @PostConstruct
     public void init(){
         System.out.println("Bean Lifecycle ::  Init method");
     }
 
-    @PreDestroy
+//    @PreDestroy
     public void destroy(){
         System.out.println("Bean Lifecycle :: Destroy method");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
     }
 }
