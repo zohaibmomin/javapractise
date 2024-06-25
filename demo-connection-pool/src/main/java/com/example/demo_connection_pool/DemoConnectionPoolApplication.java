0f397f34c04e20dc0da01b8f9a19ccbc7d47668a
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-25 20:38:11 +0530

    Springboot MySQL JDBC - Query from db

--- demo-connection-pool/src/main/java/com/example/demo_connection_pool/DemoConnectionPoolApplication.java
+++ demo-connection-pool/src/main/java/com/example/demo_connection_pool/DemoConnectionPoolApplication.java
@@ -1,12 +1,20 @@
 package com.example.demo_connection_pool;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoConnectionPoolApplication implements CommandLineRunner {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     public static void main(String[] args) {
 		SpringApplication.run(DemoConnectionPoolApplication.class, args);
     }
@@ -14,5 +22,11 @@ public class DemoConnectionPoolApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("App is started");
+        List<Map<String, Object>> result = jdbcTemplate.queryForList("select * from Customers;");
+        result.forEach(System.out::println);
+        //output
+        /*App is started
+        {Age=35, LastName=Zohaib}
+        {Age=31, LastName=Neha}*/
     }
 }
