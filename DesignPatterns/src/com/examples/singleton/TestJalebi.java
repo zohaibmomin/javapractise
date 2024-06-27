2615d8b20a88d8ea7413a27d115d52aef02aee8d
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-27 12:29:14 +0530

    Singleton - refactor

--- DesignPatterns/src/com/examples/singleton/TestJalebi.java
+++ DesignPatterns/src/com/examples/singleton/TestJalebi.java
@@ -3,9 +3,9 @@ package com.examples.singleton;
 public class TestJalebi {
     public static void main(String[] args) {
         Jalebi jalebi1 = Jalebi.getJalebi();
-        System.out.printf("Jalebi 1 is - %s", jalebi1.hashCode());
+        System.out.printf("\nJalebi 1 is - %s", jalebi1.hashCode());
 
         Jalebi jalebi2 = Jalebi.getJalebi();
-        System.out.printf("Jalebi 2 is - %s", jalebi2.hashCode());
+        System.out.printf("\nJalebi 2 is - %s", jalebi2.hashCode());
     }
 }
