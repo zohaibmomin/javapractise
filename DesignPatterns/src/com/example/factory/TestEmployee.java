5da8881496201fa03d146ec78729be5636dc60a1
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-27 15:31:52 +0530

    Factory - Null checks

--- DesignPatterns/src/com/example/factory/TestEmployee.java
+++ DesignPatterns/src/com/example/factory/TestEmployee.java
@@ -3,9 +3,11 @@ package com.example.factory;
 public class TestEmployee {
     public static void main(String[] args) {
         Employee employee = EmployeeFactory.getEmployeeClient("Android");
+        assert employee != null;
         System.out.printf("Salary is - %s", employee.salary());
 
         Employee employee2 = EmployeeFactory.getEmployeeClient("Java");
+        assert employee2 != null;
         System.out.printf("Salary is - %s",employee2.salary());
     }
 }
