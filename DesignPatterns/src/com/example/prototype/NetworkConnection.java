7e62d6c8bc72b8707d912c7ca3ccfd9e31bdd92e
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-06-28 14:29:17 +0530

    Prototype Pattern - Shallow copy sample. How changing list affects the cloned copy

--- DesignPatterns/src/com/example/prototype/NetworkConnection.java
+++ DesignPatterns/src/com/example/prototype/NetworkConnection.java
@@ -1,20 +1,29 @@
 package com.example.prototype;
 
-public class NetworkConnection implements Cloneable{
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkConnection implements Cloneable {
     private String networkIP;
-    private final static String data="Hello dummy image across 1000km";
+    private final static String data = "Hello dummy image across 1000km";
+    private List<String> domains = new ArrayList<>();
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("NetworkConnection{");
         sb.append("networkIP='").append(networkIP).append('\'');
-        sb.append(", data='").append(data).append('\'');
+        sb.append("data='").append(data).append('\'');
+        sb.append(", domains=").append(domains);
         sb.append('}');
         return sb.toString();
     }
 
     public void loadVeryImportantData() throws InterruptedException {
         System.out.println("Loading important data");
+        this.domains.add("www.google.com");
+        this.domains.add("www.zohaib.com");
+        this.domains.add("www.demosample.com");
+        this.domains.add("www.helloworld.com");
         Thread.sleep(2000);
     }
 
@@ -28,6 +37,14 @@ public class NetworkConnection implements Cloneable{
 
     public String getData() {
         return data;
+    }
+
+    public List<String> getDomains() {
+        return domains;
+    }
+
+    public void setDomains(List<String> domains) {
+        this.domains = domains;
     }
 
     @Override
--- DesignPatterns/src/com/example/prototype/TestPrototypePattern.java
+++ DesignPatterns/src/com/example/prototype/TestPrototypePattern.java
@@ -8,12 +8,17 @@ public class TestPrototypePattern {
         nw.setNetworkIP("192.56.18.99");
         nw.loadVeryImportantData();
 
-        System.out.println(nw);
+        System.out.println("OG : "+nw);
 
-        //we want new object but saving costs of not doing heavy operation in loadVeryImportantData
+        // we want new object but saving costs of not doing heavy operation in loadVeryImportantData
         // simply clone it
         NetworkConnection nw2 = nw.clone();
-        System.out.println(nw2);
+        System.out.println("OG Clone : " + nw2);
+
+        //Now we will modify something in Object List<> and demonstrate shallow copy example
+        nw.getDomains().remove(0);
+        System.out.println("OG After remove : "+nw);
+        System.out.println("OG Clone : "+nw2);
 
     }
 }
