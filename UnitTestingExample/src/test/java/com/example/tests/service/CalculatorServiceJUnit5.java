8868e7453ae02828e9ea16598cf284fcc1320c70
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-07-02 15:58:08 +0530

    Junit 5 -  @BeforeAll,@AfterAll,@BeforeEach,@AfterEach

--- UnitTestingExample/src/test/java/com/example/tests/service/CalculatorServiceJUnit5.java
+++ UnitTestingExample/src/test/java/com/example/tests/service/CalculatorServiceJUnit5.java
@@ -1,9 +1,29 @@
 package com.example.tests.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.Date;
 
 public class CalculatorServiceJUnit5 {
+    @BeforeAll
+    public static void beginning(){
+        System.out.println("Before All test cases start... + " + new Date());
+    }
+    @AfterAll
+    public static void ending(){
+        System.out.println("After All test cases end... + " + new Date());
+    }
+
+    @BeforeEach
+    public void beforeEachTestCase(){
+        System.out.println("beforeEachTestCase...");
+    }
+    @AfterEach
+    public  void afterEachTestCase(){
+        System.out.println("afterEachTestCase... ");
+    }
+
+
     @Test
     public void sumOfTwo() throws InterruptedException {
         System.out.println("Test Case :: sumOfTwo");
