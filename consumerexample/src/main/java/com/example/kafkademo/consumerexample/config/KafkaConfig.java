362df4290ff51f314e325c94789093d5ae55ce3a
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-07-05 13:16:44 +0530

    Kafka Consumer - Listen to the topic for location updates

---
+++ consumerexample/src/main/java/com/example/kafkademo/consumerexample/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.example.kafkademo.consumerexample.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+@Configuration
+public class KafkaConfig {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updateLocation(String value) {
+        this.logger.info(value);
+    }
+}
