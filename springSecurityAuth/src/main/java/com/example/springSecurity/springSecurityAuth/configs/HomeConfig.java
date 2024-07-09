67202de6364ab9fd3c5afe9a0973924e5f3fbe55
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-07-09 13:58:52 +0530

    Spring Security - Method level access control imposed on Routes

--- springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/configs/HomeConfig.java
+++ springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/configs/HomeConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class HomeConfig {
 
     @Bean
@@ -41,9 +43,9 @@ public class HomeConfig {
 
         httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
-                        auth.
-                                requestMatchers("/home/admin").hasRole("ADMIN")
-                                .requestMatchers("/home/normal").hasRole("NORMAL")
+                        auth
+//                                requestMatchers("/home/admin").hasRole("ADMIN")
+//                                .requestMatchers("/home/normal").hasRole("NORMAL")
                                 .requestMatchers("/home/public")
                                 .permitAll()
                                 .anyRequest()
--- springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/controllers/HomeController.java
+++ springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.springSecurity.springSecurityAuth.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public ResponseEntity<String> adminUser() {
         return ResponseEntity.ok("Hello, I am admin user");
@@ -19,6 +21,7 @@ public class HomeController {
         return ResponseEntity.ok("Hello, I am public user");
     }
 
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping("/normal")
     public ResponseEntity<String> normalUser() {
         return ResponseEntity.ok("Hello, I am normal user");
