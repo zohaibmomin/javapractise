7f13491962b8ae876db70485621a69e3d506c6e8
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-07-09 13:46:07 +0530

    Spring Security - Defining Access Control Role based

--- springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/configs/HomeConfig.java
+++ springSecurityAuth/src/main/java/com/example/springSecurity/springSecurityAuth/configs/HomeConfig.java
@@ -5,20 +5,50 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class HomeConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-         httpSecurity.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth-> auth.requestMatchers("/home/public")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.withUsername("normal")
+                .password(passwordEncoder().encode("abc@123"))
+                .roles("NORMAL")
+                .build();
+
+        UserDetails adminUser = User.withUsername("admin")
+                .password(passwordEncoder().encode("abc@123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.
+                                requestMatchers("/home/admin").hasRole("ADMIN")
+                                .requestMatchers("/home/normal").hasRole("NORMAL")
+                                .requestMatchers("/home/public")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
                 .formLogin(Customizer.withDefaults());
-         return httpSecurity.build();
+        return httpSecurity.build();
     }
 }
