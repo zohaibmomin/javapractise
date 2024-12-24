fbe6a0701842a7e96feeca63a90229d536ae2d74
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2024-12-24 18:22:53 +0530

    Solid principles - InterfaceSegmented principle VOILATION example

---
+++ JavaPractise/CodingChallenges/src/com/solidprinciples/interfaceSegmentedPrinciple/InterfaceRestaurantEmployee.java
@@ -0,0 +1,9 @@
+package com.solidprinciples.interfaceSegmentedPrinciple;
+
+public interface InterfaceRestaurantEmployee {
+    void serveFood();
+
+    void cookFood();
+
+    void washUtensils();
+}
---
+++ JavaPractise/CodingChallenges/src/com/solidprinciples/interfaceSegmentedPrinciple/Waiter.java
@@ -0,0 +1,21 @@
+package com.solidprinciples.interfaceSegmentedPrinciple;
+
+//VOILATION
+//Waiter cannot cook and washutensils
+//Break the Parent Interface to relevant small interfaces
+public class Waiter implements InterfaceRestaurantEmployee{
+    @Override
+    public void serveFood() {
+
+    }
+
+    @Override
+    public void cookFood() {
+
+    }
+
+    @Override
+    public void washUtensils() {
+
+    }
+}
