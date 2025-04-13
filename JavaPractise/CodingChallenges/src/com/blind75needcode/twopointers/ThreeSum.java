bea055be26b17774991530ca9d1b12fac45bffa4
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2025-04-13 15:40:41 +0530

    NeedCodeBlind75-TwoPointers::3Sum

--- JavaPractise/CodingChallenges/src/com/blind75needcode/twopointers/ThreeSum.java
+++ JavaPractise/CodingChallenges/src/com/blind75needcode/twopointers/ThreeSum.java
@@ -2,6 +2,20 @@ package com.blind75needcode.twopointers;
 
 import java.util.*;
 
+/*
+* https://neetcode.io/problems/three-integer-sum
+* 3Sum
+Solved
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+
+The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+
+Output: [[-1,-1,2],[-1,0,1]]
+* */
 public class ThreeSum {
     public static void main(String[] args) {
 
@@ -48,7 +62,7 @@ public class ThreeSum {
                 } else if (sum < 0) {
                     //sum is small so increase left
                     left++;
-                } else if (sum > 0) {
+                } else{
                     //sum is large so decrease right
                     right--;
                 }
