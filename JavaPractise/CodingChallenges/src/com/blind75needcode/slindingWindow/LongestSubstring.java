5a58b3c9e26c6e894360553886668283be8ca47e
zohaibmomin <naziamomin@Nazias-MacBook-Air.local>
2025-04-19 14:09:32 +0530

    NeedCodeBlind75-SlidingWindow::LongestSubstring

--- JavaPractise/CodingChallenges/src/com/blind75needcode/slindingWindow/LongestSubstring.java
+++ JavaPractise/CodingChallenges/src/com/blind75needcode/slindingWindow/LongestSubstring.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 * Given a string s, find the length of the longest substring without duplicate characters.
 A substring is a contiguous sequence of characters within a string.
 Example 1:
-Input: s = "zxyzxyz"
+Input: s = "zxyzxyz".
 Output: 3*/
 public class LongestSubstring {
 
