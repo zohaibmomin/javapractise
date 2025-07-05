9558916a0d339bcbdb7a0c0ff9c0aa0b1f7748aa
zohaibmomin <zohaibrulz@gmail.com>
2025-07-05 12:51:02 +0530

    Logwatcher Scratch - Part 1

---
+++ logwatcherscratch/src/main/java/com/example/logwatcherscratch/LogWatcherScratch.java
@@ -0,0 +1,14 @@
+package com.example.logwatcherscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogWatcherScratch {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(LogWatcherScratch.class, args);
+
+
+    }
+}
